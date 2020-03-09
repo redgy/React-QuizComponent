620d4529fa0bc2a42401f95ab5565e557e518c03
rcanos <Redgy.Canos@viasat.com>
2020-03-09 14:03:41 -0700

    Module 2 Part 6

--- src/Quiz.js
+++ src/Quiz.js
@@ -11,9 +11,7 @@ class Quiz extends Component {
 
     render() {
         return <div>j
-            <div className="QuizQuestion">
-                {quizData.quiz_questions[0].instruction_text}
-            </div>            
+            <QuizQuestion />
         </div>
     }
 }
