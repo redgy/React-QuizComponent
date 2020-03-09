48e79810853b17fc35b2343fb98f24c558fdf944
rcanos <Redgy.Canos@viasat.com>
2020-03-09 14:13:20 -0700

    Module 4 Part 4

--- src/QuizQuestionButton.js
+++ src/QuizQuestionButton.js
@@ -2,6 +2,11 @@ import React, { Component } from 'react'
 
 class QuizQuestionButton extends Component {
 
+    render() {
+        return <li>
+            <button />
+        </li>
+    }
 }
 
 export default QuizQuestionButton
