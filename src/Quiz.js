40fee50d639ded6d2257fde029cfb8e9426494ed
rcanos <Redgy.Canos@viasat.com>
2020-03-09 13:51:01 -0700

    Fix typo 'class' -> 'className'

--- src/Quiz.js
+++ src/Quiz.js
@@ -10,7 +10,7 @@ class Quiz extends Component {
 
     render() {
         return <div>j
-            <div class="QuizQuestion">
+            <div className="QuizQuestion">
                 {quizData.quiz_questions[0].instruction_text}
             </div>            
         </div>
