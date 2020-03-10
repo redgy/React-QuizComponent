283a187d194202a4befe36d311b750eaaf40bafc
rcanos <Redgy.Canos@viasat.com>
2020-03-10 11:52:51 -0700

    Add reset funcitonality

--- src/Quiz.js
+++ src/Quiz.js
@@ -10,6 +10,8 @@ class Quiz extends Component {
         this.state = {quiz_position: 1}
     }
 
+    handleResetClick() { this.setState({quiz_position: 1}) }
+
     showNextQuestion() { 
         this.setState((state) => {
             return { quiz_position: state.quiz_position+1}
@@ -20,7 +22,7 @@ class Quiz extends Component {
         const isQuizEnd = ((this.state.quiz_position - 1) === quizData.quiz_questions.length)
         return (
           <div>
-            {isQuizEnd ? <QuizEnd /> : <QuizQuestion quiz_question={quizData.quiz_questions[this.state.quiz_position - 1]} showNextQuestionHandler={this.showNextQuestion.bind(this)} />}
+            {isQuizEnd ? <QuizEnd resetClickHandler={this.handleResetClick.bind(this)}/> : <QuizQuestion quiz_question={quizData.quiz_questions[this.state.quiz_position - 1]} showNextQuestionHandler={this.showNextQuestion.bind(this)} />}
           </div>
         )
     }
