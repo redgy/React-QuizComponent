cdf0d522eb5a4d33f1cbd2b597d95abca2db53af
rcanos <Redgy.Canos@viasat.com>
2020-03-10 11:32:13 -0700

    Set up handlers in Quiz.js

--- src/Quiz.js
+++ src/Quiz.js
@@ -10,11 +10,15 @@ class Quiz extends Component {
         this.state = {quiz_position: 1}
     }
 
+    showNextQuestion() {
+
+    }
+
     render() {
         const isQuizEnd = ((this.state.quiz_position - 1) === quizData.quiz_questions.length)
         return (
           <div>
-            {isQuizEnd ? <QuizEnd /> : <QuizQuestion quiz_question={quizData.quiz_questions[this.state.quiz_position - 1]} />}
+            {isQuizEnd ? <QuizEnd /> : <QuizQuestion quiz_question={quizData.quiz_questions[this.state.quiz_position - 1]} showNextQuestionHandler={this.showNextQuestion.bind(this)} />}
           </div>
         )
     }
