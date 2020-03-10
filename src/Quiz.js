897dd011a504267df4924f4e8c80f3f5df33454e
rcanos <Redgy.Canos@viasat.com>
2020-03-10 07:26:24 -0700

    Fix typo

--- src/Quiz.js
+++ src/Quiz.js
@@ -11,7 +11,7 @@ class Quiz extends Component {
     }
 
     render() {
-        const isQuizEnd = quizData.quiz_questions.length === this.state.quiz_position - 1
+        const isQuizEnd = quizData.quiz_questions.length === (this.state.quiz_position - 1)
         return <div>j
             { isQuizEnd ? <QuizEnd /> : <QuizQuestion quiz_question={quizData.quiz_questions[this.state.quiz_position - 1]} /> }
         </div>
