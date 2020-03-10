42b8ed9f4154a062774dbfb958381d91629083d0
rcanos <Redgy.Canos@viasat.com>
2020-03-10 11:49:19 -0700

    Add p tag to error

--- src/QuizQuestion.js
+++ src/QuizQuestion.js
@@ -10,13 +10,9 @@ class QuizQuestion extends Component {
     handleClick(buttonText) {
         if (buttonText === this.props.quiz_question.answer) { 
             this.props.showNextQuestionHandler() 
-            this.setState((state) => {
-                return { incorrectAnswer: false }
-            })
+            this.setState({ incorrectAnswer: false })
         } else {
-            this.setState((state) => {
-                return { incorrectAnswer: true }
-            })
+            this.setState({ incorrectAnswer: true })
         }
     }
 
@@ -33,7 +29,7 @@ class QuizQuestion extends Component {
                 })}
             </ul>
             </section>
-            {this.state.incorrectAnswer ? "Sorry that's not right" : null}
+            {this.state.incorrectAnswer ? <p className='error'>Sorry, that's not right</p> : null}
         </main>
     }
 
