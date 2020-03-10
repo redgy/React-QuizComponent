da409fab55243cc2ec2e45263edc300bc82e6dc8
rcanos <Redgy.Canos@viasat.com>
2020-03-10 11:23:24 -0700

    Add click handler skeleton

--- src/QuizQuestion.js
+++ src/QuizQuestion.js
@@ -2,6 +2,10 @@ import React, { Component } from 'react'
 import QuizQuestionButton from './QuizQuestionButton.js'
 
 class QuizQuestion extends Component {
+    clickHandler = this.handleClick.bind(this)
+
+    handleClick(buttonText) {
+    }
 
     render() {
         return <main>
@@ -10,11 +14,11 @@ class QuizQuestion extends Component {
             </section>
 
             <section className="buttons">
-                <ul>
-                    <li>
-                        <QuizQuestionButton button_text={this.props.quiz_question.answer_options[0]}/>
-                    </li>
-                </ul>
+            <ul>
+                {this.props.quiz_question.answer_options.map((answer_option, index) => {
+                return <QuizQuestionButton key={index} button_text={answer_option} />
+                })}
+            </ul>
             </section>
         </main>
     }
